50bd1e18f0edaa72d24d1ed92185ec13d3eba66e
Daniel Curran <daniel.curran@RACKSPACE.com>
2015-02-16 15:38:14 -0600

    trying some new db stuff

--- app/db/models.py
+++ app/db/models.py
@@ -6,29 +6,32 @@ class User(db.Model):
     username = db.Column(db.String(80))
     tenant_id = db.Column(db.String(80))
     user_id = db.Column(db.String(80))
+    default_auth_url = db.Column(db.String(80))
+    trusts = db.relationship('Trusts', backref=db.backref('user', lazy='dynamic'))
 
-    def __init__(self, username, tenant_id, user_id):
+    def __init__(self, username, tenant_id, user_id, default_auth_url, group):
         self.username = username
         self.tenant_id = tenant_id
         self.user_id = user_id
-
+        self.default_auth_url = default_auth_url
     def __repr__(self):
-        return '<Username %s. Tenant ID %s. User ID %s>' % (self.username, self.tenant_id, self.user_id)
+        return '<Username %s. Tenant ID %s. Keystone %s.>' % (self.username, self.tenant_id, self.default_auth_url)
 
     def is_authenticated(self):
         return True
 
-
-class Trust(db.Model):
+class Keystone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    trustor_username = db.Column(db.String(80))
-    trustee_username = db.Column(db.String(80))
-    trust_id = db.Column(db.String(80), unique=True)
+    auth_url = db.Column(db.String(80))
 
-    def __init__(self, trustor_username, trustee_username, trust_id):
-        self.trustor_username = trustor_username
-        self.trustee_username = trustee_username
-        self.trust_id = trust_id
+    def __init__(self, auth_url):
+        self.auth_url = auth_url
+
+class Trusts(db.Model):
+    user_fk = Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    keystone_fk = Column(db.Integer, db.ForeignKey('keystone.id'), primary_key=True)
+    trust_id = Column(db.String(80), unique=True)
+    keystone = relationship('Keystone', backref='trusts')
 
     def __repr__(self):
-        return '<Trustor User %s. Trustee User %s. Trust ID %s>' % (self.trustor_username, self.trustee_username, self.trust_id)
+        return '<Trust ID %s. User ID %s. Keystone ID %s.>' % (self.trust_id, self.user_fk, self.keystone_fk)
