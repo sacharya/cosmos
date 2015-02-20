490b681cea92942d81d38a77e5f5d0768d8fd725
Daniel Curran <daniel.curran@RACKSPACE.com>
2015-02-20 12:10:41 -0600

    adding proper dbapi call

--- app/db/dbapi.py
+++ app/db/dbapi.py
@@ -15,13 +15,14 @@ def get_all_trusts_by_trustor(trustor_username):
     return Trust.query.filter_by(trustor_username=trustor_username).all()
 
 
-def create_or_get_user(username, tenant_id, user_id, default_auth_url):
+def create_or_get_user(username, tenant_id, user_id, default_region, default_auth_url):
     u = User.query.filter_by(username=username, 
                              default_auth_url=default_auth_url).first()
     if u == None:
         u = User(username=username, 
                  tenant_id=tenant_id, 
                  user_id=user_id, 
+                 default_region=default_region,
                  default_auth_url=default_auth_url)
         db.session.add(u)
         db.session.commit()
